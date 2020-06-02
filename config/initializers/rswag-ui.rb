a3d68b17d18b3214fe08ebbc12e945073a58298d
arubinofaux <arubinofaux@gmail.com>
2020-06-02 00:19:21 -0400

    authenticate swagger

--- config/initializers/rswag-ui.rb
+++ config/initializers/rswag-ui.rb
@@ -9,6 +9,6 @@ Rswag::Ui.configure do |c|
   c.swagger_endpoint '/api-docs/v1/swagger.yaml', 'API V1 Docs'
 
   # Add Basic Auth in case your API is private
-  # c.basic_auth_enabled = true
-  # c.basic_auth_credentials 'username', 'password'
+  c.basic_auth_enabled = true
+  c.basic_auth_credentials ENV["SWAGGER_USERNAME"], ENV["SWAGGER_PASSWORD"]
 end
